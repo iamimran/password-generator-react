3af708416e3aefb42d64d1d9eb7d0f453266ffcd
Imran Ahmed <imran@192.168.1.2>
2023-11-19 16:48:02 +0500

    created UI and integrated it with react and javascript to have working password generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,99 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./assets/css/index.css";
+
 function App() {
-  return <></>;
+  const [length, setLength] = useState(8);
+  const [numbersAllowed, setNumbersAllowed] = useState(false);
+  const [charsAllowed, setCharsAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let password = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numbersAllowed) str += "01234567890";
+    if (charsAllowed) str += "!@#$%^&*()~[]{}|?;'><.,/";
+
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * str.length);
+      password += str.charAt(index);
+    }
+    setPassword(password);
+  }, [length, numbersAllowed, charsAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numbersAllowed, charsAllowed, passwordGenerator]);
+
+  const passwordRef = useRef(null);
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 mt-3"
+            placeholder="generated password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 mt-3 shrink-0"
+            onClick={copyPasswordToClipboard}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={1}
+              max={50}
+              value={length}
+              className="cursor-pointer"
+              id="passwordLength"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="passwordLength">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numbersAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumbersAllowed((prevValue) => !prevValue);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charsAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharsAllowed((prevValue) => !prevValue);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
